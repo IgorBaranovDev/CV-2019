851d1c2ab7dbecf7ce610e0d17d41f151e3f853c
Baranov Igor <igor.82bv@mail.ru>
2019-07-31 19:26:35 +0300

    fix: send.js

--- src/scripts/send.js
+++ src/scripts/send.js
@@ -1,12 +1,23 @@
-function sendMessege() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "user_name",
-    To: 'igor.82bv@mail.ru',
-    From: "you@isp.com",
-    Subject: "user_email",
-    Body: "user_message"
-  }).then(
-    message => alert('Mail send!')
-  );
-};
+$(function () {
+  document.getElementById('form-send').addEventListener('submit', function (evt) {
+    var http = new XMLHttpRequest(), f = this;
+    var th = $(this);
+    evt.preventDefault();
+    http.open("POST", "contact.php", true);
+    http.onreadystatechange = function () {
+      if (http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+        console.log('send');
+
+        if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+          th.trigger("reset");
+        }
+      }
+    }
+    http.onerror = function () {
+      alert('Ошибка, попробуйте еще раз');
+    };
+    http.send(new FormData(f));
+  }, false);
+
+});
