17a0c5318fc4ce241091a5745ab65edc9a12e8fc
Ihar Baranau <baranovigorjsdev@gmail.com>
2025-01-20 17:34:14 +0100

    update foto

--- src/scripts/hiddenHeader.js
+++ src/scripts/hiddenHeader.js
@@ -7,15 +7,28 @@
 //     }
 // });
 
-$(document).on('mousemove', function (event) {
-    if (event.clientY <= 80 || event.pageY <= 640) {
-        $('header').removeClass('header none')
-                   .addClass('header active');
-    } else {
-        $('header').removeClass('header active')
-        .addClass('header none');
-    }
+$(document).on("mousemove", function (event) {
+  if (event.clientY <= 80 || event.pageY <= 640) {
+    $("header").removeClass("header none").addClass("header active");
+  } else {
+    $("header").removeClass("header active").addClass("header none");
+  }
 });
+
+// window.onload = function() {
+//     const header = document.getElementById('menu');
+//   onmousemove = function(event){ 
+//     if (event.clientY <= 80 || event.pageY <= 640) {    
+//       header.style.opacity = 1;
+//       // header.classList.add('active');
+//       // header.classList.remove('none');
+//       } else {      
+//         header.style.opacity = 0;
+//         // header.classList.remove("active");
+//         // header.classList.add("none");
+//       }}
+// }
+
 
 // $('#menu').mouseout(function () {
 //     $('header').removeClass('header active')
